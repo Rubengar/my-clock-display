c073083d41db02ea09afe72c461e0ecc56f53a56
unknown <you@example.com>
2015-11-13 11:50:05 +0100

    añadido el metodogetDisplayValue

--- NumberDisplay.java
+++ NumberDisplay.java
@@ -12,18 +12,40 @@ public class NumberDisplay
     private int max;
     
     private int min;
+   
+    
     /**
      * Un constructor que recibe por parámetro el límite del display y que fija el valor actual del display a 0.
      */
     public NumberDisplay(int limiteDelDisplay)
     {
-       int valorDelDisplay = 0;
-       min = valorDelDisplay;
+       min = 0 ;
        max = limiteDelDisplay;
     }
     
+    /**
+     * fija el valor actual del display al valor pasado como parámetro
+     */
     public void setValue(int valorPasado)
     {
         min = valorPasado;
+    }
+    /**
+     * 
+     */
+    public String getDisplay()
+    {
+     String valor = "";
+        if (min < 10)
+        {
+         valor = ""+"0"+ min ;
+         return valor;
+        }
+        else
+        {
+          valor = " " + min ;
+          return valor;
+        }
+     
     }
 }
