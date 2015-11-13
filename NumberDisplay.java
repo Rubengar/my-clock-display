6ee8e7b9074528dd907c6962483ea7a23c1eb189
unknown <you@example.com>
2015-11-13 11:02:20 +0100

    Añadido el constructor

---
+++ NumberDisplay.java
@@ -0,0 +1,24 @@
+
+/**
+ * Write a description of class NumberDisplay here.
+ * 
+ * @author (your name) 
+ * @version (a version number or a date)
+ */
+public class NumberDisplay
+{
+    // instance variables - replace the example below with your own
+
+    private int max;
+    
+    private int min;
+    /**
+     * Un constructor que recibe por parámetro el límite del display y que fija el valor actual del display a 0.
+     */
+    public NumberDisplay(int limiteDelDisplay)
+    {
+       int valorDelDisplay = 0;
+       min = valorDelDisplay;
+       max = limiteDelDisplay
+    }
+}
