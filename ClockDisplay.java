a5fee5a41869182df7ac4ac920bc3ae4c05df84d
unknown <you@example.com>
2015-11-16 11:05:23 +0100

    creados los contructores de la clase NumberDisplay

--- ClockDisplay.java
+++ ClockDisplay.java
@@ -9,15 +9,26 @@ public class ClockDisplay
 {
     // instance variables - replace the example below with your own
     private NumberDisplay horas;
+    
     private NumberDisplay minutos;
+    
+    private String pantalla;
 
     /**
      * Constructor for objects of class ClockDisplay
      */
     public ClockDisplay()
+    {   
+       int horas = 00;
+       int minutos = 00;
+       pantalla = "0"+ horas + ":0" + minutos;
+    }
+    public ClockDisplay(int valorHoras, int valorMinutos)
     {
-        // initialise instance variables
+        int horas = valorHoras;
+        int minutos = valorMinutos;
        
+        
     }
 
     
